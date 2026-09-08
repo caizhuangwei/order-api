62912ee91d2971bcca3cba97b97b21946acbbbf9
caizhuangwei <368666516@qq.com>
2026-09-09 07:28:30 +0800

    Update [code].js

--- functions/o/[code].js
+++ functions/o/[code].js
@@ -1,1 +1,14 @@
+export async function onRequestGet({ request, params }) {
+  const oid = params.code;
 
+  if (!oid) {
+    return new Response('Not Found', { status: 404 });
+  }
+
+  const target = new URL(request.url);
+  target.pathname = '/adminindex.html';
+  target.search = '';
+  target.searchParams.set('oid', oid);
+
+  return Response.redirect(target.toString(), 302);
+}
